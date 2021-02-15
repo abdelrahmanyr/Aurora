5e207802c47ff8b4ba4f4bf4ae9ab1f3f0361ceb
abdelrahmanyr <71361116+abdelrahmanyr@users.noreply.github.com>
2021-02-15 21:24:25 +0200

    Update

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import kivy
 from pytube import YouTube
 from youtubesearchpython import VideosSearch
 import moviepy.editor as mp
 import os
-import eyeD3 #We will use that later
 
 #Defining spotify credentials (ignore it)
 st = "67587c0f933aa8ab2e59377a14d0d315"
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id = "4d35b62383e543679384be5c9ff3fd6a",
                                                            client_secret = "100c64fa520d4f98969c5b1bfdd92e46",))
+
+#A function for removing illegal characters
+def remove(value, deletechars):
+    for c in deletechars:
+        value = value.replace(c,'')
+    return value;
 
 #Giving the track name, album name, artist and album cover (ignore it too)
 track_id = input("Please type a Spotify track URL or ID:\n")
@@ -25,15 +29,16 @@ track_artist = track['album']['artists'][0]['name']
 album_cover = track['album']['images'][0]['url']
 
 #Now search YouTube for the song with `"track_name - track_artist"` and download it as an MP3 file
-result = VideosSearch(f"{track_name} - {track_artist}", limit = 1).result()
+result = VideosSearch(f"{track_name} - {track_artist} Audio", limit = 1).result()
 link = result['result'][0]['link'] #Now use this one to download the track with pytube
 
 #Your Turn !!!
 yt_track = YouTube(link).streams.filter().first()
-yt_track.download(filename = f"{track_name} - {track_artist}")
+correct_name = remove(f"{track_name} - {track_artist}", "'\/:*?<>|")
+yt_track.download(filename = correct_name)
 
-clip = mp.VideoFileClip(f"{yt_track.title}.mp4")
-clip.audio.write_audiofile(f"{yt_track.title}.mp3")
+clip = mp.VideoFileClip(f"{correct_name}.mp4")
+clip.audio.write_audiofile(f"{correct_name}.mp3")
 clip.close()
-os.remove(f"{yt_track.title}.mp4")
+os.remove(f"{correct_name}.mp4")
 
